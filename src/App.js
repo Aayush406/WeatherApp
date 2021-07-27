dab1bb0240c4d7ec160aed341400a1a932c6fdc0
Aayush406 <aayushbokil@gmail.com>
2021-07-27 00:50:49 -0700

    Api Implementation (first try)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
 import SearchBar from "./SearchBar.js";
+import {useState} from "react";
+
+const apiInfo = {
+  key: "718bc4c83b7278c32f27be07e724e39a",
+  base: "api.openweathermap.org/data/2.5/weather?"
+}
+
 
 function App() {
+  
+  const [city, setCity] = useState("");
+  const [currentWeather, setCurrentWeather] = useState("");
+  
+  const getCity = (cityName) =>{
+    setCity(cityName);
+    getWeatherInfo();
+  }
+
+  const getWeatherInfo = async() =>{
+    const response = await fetch(apiInfo.base + "q=" + city + "&appid=" + apiInfo.key);
+
+    if (response.status === 200) {
+      console.log(response);
+      console.log(response.json());
+      // const weather = await response.json()
+      // setCurrentWeather(weather);
+      // console.log(weather);
+    }
+    else{
+      throw new Error("Unable to get weather");
+    }
+  //   fetch('${apiInfo.base}q=${city}&appid=${apiInfo.key}')
+  //     .then(response => response.json())
+  //     .then(weather => setCurrentWeather(weather));
+  }
+  
   return (
     <div className="App">
-      <SearchBar></SearchBar>
+      <SearchBar getCityName={getCity}></SearchBar>
+      <div>
+        
+      </div>
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -6,12 +6,20 @@ function SearchBar(props){
     const [cityName, setCityName] = useState("");
 
 
+    const giveName = () =>{
+        if(cityName !== ""){
+            props.getCityName(cityName);
+        }
+        setCityName("");
+    } 
+
+
     return(
         <div>
             <h2>Find Current Weather Conditions</h2>
             <form>
-                <input type="text"/>
-                <button>Get Forecast</button>
+                <input type="text" placeholder="Enter City Name" value={cityName} onChange={(e) => setCityName(e.target.value)}/>
+                <button type="button" onClick={giveName}>Get Forecast</button>
             </form> 
             {/* <button>Get Forecast</button> */}
         </div>
