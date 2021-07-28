554c38f829bda60a1455cb4be488b0d2444e5f78
Aayush406 <aayushbokil@gmail.com>
2021-07-27 21:16:19 -0700

    API Data received

--- src/App.js
+++ src/App.js
@@ -2,45 +2,37 @@ import './App.css';
 import SearchBar from "./SearchBar.js";
 import {useState} from "react";
 
+
 const apiInfo = {
   key: "718bc4c83b7278c32f27be07e724e39a",
-  base: "api.openweathermap.org/data/2.5/weather?"
+  base: "http://api.openweathermap.org/data/2.5/weather?"
 }
 
 
 function App() {
   
-  const [city, setCity] = useState("");
+  // const [city, setCity] = useState("Irvine");
   const [currentWeather, setCurrentWeather] = useState("");
-  
-  const getCity = (cityName) =>{
-    setCity(cityName);
-    getWeatherInfo();
-  }
 
-  const getWeatherInfo = async() =>{
+  const getWeatherInfo = async(city) =>{
+    var link = (apiInfo.base + "q=" + city + "&appid=" + apiInfo.key);
+    console.log(link)
     const response = await fetch(apiInfo.base + "q=" + city + "&appid=" + apiInfo.key);
 
     if (response.status === 200) {
-      console.log(response);
-      console.log(response.json());
-      // const weather = await response.json()
-      // setCurrentWeather(weather);
-      // console.log(weather);
+      const weather = await response.json()
+      setCurrentWeather(weather);
+      console.log(weather);
     }
     else{
       throw new Error("Unable to get weather");
     }
-  //   fetch('${apiInfo.base}q=${city}&appid=${apiInfo.key}')
-  //     .then(response => response.json())
-  //     .then(weather => setCurrentWeather(weather));
   }
-  
   return (
     <div className="App">
-      <SearchBar getCityName={getCity}></SearchBar>
+      <SearchBar getCityWeather={getWeatherInfo}></SearchBar>
       <div>
-        
+        <p>{parseInt((currentWeather.main.temp - 273) * 9/5 + 32)}</p>
       </div>
     </div>
   );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,10 +5,18 @@ import {useState} from "react";
 function SearchBar(props){
     const [cityName, setCityName] = useState("");
 
+    function handleSubmit(e){
+        e.preventDefault();
+        if(cityName !== "" ){
+            props.getCityWeather(cityName);
+        }
+        setCityName("");
+    }
+
 
     const giveName = () =>{
-        if(cityName !== ""){
-            props.getCityName(cityName);
+        if(cityName !== "" ){
+            props.getCityWeather(cityName);
         }
         setCityName("");
     } 
@@ -17,7 +25,7 @@ function SearchBar(props){
     return(
         <div>
             <h2>Find Current Weather Conditions</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Enter City Name" value={cityName} onChange={(e) => setCityName(e.target.value)}/>
                 <button type="button" onClick={giveName}>Get Forecast</button>
             </form> 
